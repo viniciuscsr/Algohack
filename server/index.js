f0980ef3ca29e45b3d685211347f303edfbe5022
Vinicius Rodrigues <44007400+viniciuscsr@users.noreply.github.com>
2022-08-25 15:40:43 -0400

    connect backend and frontend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 const request = require('request');
 const fs = require('fs');
 const jsdom = require('jsdom');
@@ -17,8 +17,17 @@ request('https://news.ycombinator.com/news', (error, response, body) => {
   console.log(dom.window.document.querySelector('div').textContent);
 });
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello world');
+});
+
+app.post('/api/airbnb', (req, res) => {
+  console.log(req.body);
+  res.send({
+    test1: 'test1',
+  });
 });
 
 app.listen(port, () => {
